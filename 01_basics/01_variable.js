638fb07e2cf0d6e9b57ddbee2d54e353b8e7e3eb
ABHAY YADUVANSHI <systummm0901@gmail.com>
2024-10-04 11:06:52 +0000

    hii

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail = "hii123@gmail.com"
+var accountPassword = "09876"
+accountCity = "lko"
+
+// accountId = 2
+accountEmail = "asd123@gmail.com"
+accountPassword = "213243"
+accountCity = "ftp"
+
+// console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
